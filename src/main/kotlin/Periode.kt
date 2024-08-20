2d35856885f593fd076d84f7be0c979065177a03
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-08-20 08:40:58 +0200

    Stjeler hele Periode fra Spøklse som stjal fra Spleis :)
    https://github.com/navikt/helse-spokelse/commit/6cddad392f5b2e208de6d69430e442326460b174

--- src/main/kotlin/Periode.kt
+++ src/main/kotlin/Periode.kt
@@ -1,14 +1,15 @@
 import java.time.LocalDate
 
-class Periode(
-    fom: LocalDate,
-    tom: LocalDate
-): ClosedRange<LocalDate>{
-    init { check(tom >= fom) { "$fom til $tom var jo en tøysete periode da."} }
+internal class Periode(fom: LocalDate, tom: LocalDate): ClosedRange<LocalDate>, Iterable<LocalDate> {
 
-    override val start = fom
-    override val endInclusive = tom
-    override fun toString() = "$start til $endInclusive"
+    override val start: LocalDate = fom
+    override val endInclusive: LocalDate = tom
+
+    init {
+        require(start <= endInclusive) { "fom ($start) kan ikke være etter tom ($endInclusive)" }
+    }
+
+    internal fun overlapperMed(other: Periode) = overlappendePeriode(other) != null
 
     private fun overlappendePeriode(other: Periode): Periode? {
         val start = maxOf(this.start, other.start)
@@ -17,14 +18,44 @@ class Periode(
         return Periode(start, slutt)
     }
 
-    private fun overlapperMed(other: Periode) = overlappendePeriode(other) != null
+    private fun merge(other: Periode): Periode {
+        if (this.overlapperMed(other) || this.endInclusive.plusDays(1) == other.start || other.endInclusive.plusDays(1) == this.start) {
+            return this + other
+        }
+        return this
+    }
 
-    companion object {
-        fun Iterable<Periode>.overlapper(): Boolean {
-            sortedBy { it.start }.zipWithNext { nåværende, neste ->
-                if (nåværende.overlapperMed(neste)) return true
+    override operator fun iterator() = object : Iterator<LocalDate> {
+        private var currentDate: LocalDate = start
+
+        override fun hasNext() = endInclusive >= currentDate
+
+        override fun next() =
+            currentDate.also { currentDate = it.plusDays(1) }
+    }
+
+    operator fun contains(other: Periode) =
+        this.start <= other.start && this.endInclusive >= other.endInclusive
+
+    operator fun plus(annen: Periode?): Periode {
+        if (annen == null) return this
+        return Periode(minOf(this.start, annen.start), maxOf(this.endInclusive, annen.endInclusive))
+    }
+
+    override fun equals(other: Any?) = other is Periode && this.start == other.start && this.endInclusive == other.endInclusive
+    override fun hashCode() = start.hashCode() * 37 + endInclusive.hashCode()
+
+    internal companion object {
+        internal fun Iterable<Periode>.grupperSammenhengendePerioder(): List<Periode> {
+            val resultat = mutableListOf<Periode>()
+            val sortert = sortedBy { it.start }
+            sortert.forEachIndexed { index, periode ->
+                if (resultat.any { champion -> periode in champion }) return@forEachIndexed // en annen periode har spist opp denne
+                resultat.add(sortert.subList(index, sortert.size).reduce { champion, challenger ->
+                    champion.merge(challenger)
+                })
             }
-            return false
+            return resultat
         }
     }
 }
