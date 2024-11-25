b32590019d2b418beb2e2818fde4dd047eb4e1c0
david steinsland <david@davidsteinsland.net>
2024-11-25 10:29:13 +0100

    bumper tbd-libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ val postgresqlVersion = "42.7.4"
 val kotliqueryVersion = "1.9.0"
 val hikariCPVersion = "6.1.0"
 val junitJupiterVersion = "5.11.3"
-val tbdLibsVersion = "2024.11.24-12.01-42fdc22d"
+val tbdLibsVersion = "2024.11.25-10.23-348b187b"
 val mockkVersion = "1.13.12"
 
 repositories {
