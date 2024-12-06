524a0ffc958af7739938be08ebbdbb70046b533b
david steinsland <david@davidsteinsland.net>
2024-12-06 09:02:32 +0100

    bumper r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "2.0.21"
 }
 
-val rapidsAndRiversVersion = "2024112511071732529266.253c42b70448"
+val rapidsAndRiversVersion = "2024120608421733470968.fc0a3d5e3685"
 val flywayCoreVersion = "10.21.0"
 val postgresqlVersion = "42.7.4"
 val kotliqueryVersion = "1.9.0"
