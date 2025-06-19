3a923901be0dbc94b7dc24ad8e4d1805f8ed6ea4
david steinsland <david@davidsteinsland.net>
2025-06-19 08:43:05 +0200

    bumper r&r og legger til team-logs

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "2.1.20"
 }
 
-val rapidsAndRiversVersion = "2025033014191743337188.2f9d6b08d096"
+val rapidsAndRiversVersion = "2025061811051750237542.df739400e55e"
 val flywayCoreVersion = "11.5.0"
 val postgresqlVersion = "42.7.5"
 val kotliqueryVersion = "1.9.0"
